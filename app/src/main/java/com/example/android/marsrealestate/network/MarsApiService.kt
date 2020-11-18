b6aad69abc71a31366a7050e7f6a6ad93633c730
Maxim Grachev <mxmgrachev@gmail.com>
2020-11-18 21:30:45 +0300

    JSON from service can be recieved

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,25 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.Call
+
 private const val BASE_URL = "https://mars.udacity.com/"
+
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+interface MarsApiService {
+    @GET("realestate")
+    fun getProgerties(): Call<String>
+}
+
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
